5a68d57cc8b36c08f0e66b1bf8e7babb63eafe2b
Abhishek <abhishekakkannavar@gmail.com>
2026-05-12 16:35:25 +0530

    Add scripts related to LLM and kafka

--- main.py
+++ main.py
@@ -21,3 +21,11 @@ a += 2
 print(a)
 print(a -+ 2)
 
+from -> join -> where -> group by -> having -> select -> limit
+# Write your MySQL query statement below
+SELECT DISTINCT num ConsecutiveNums from LOGS WHERE (id, num) in (SELECT id+1, num FROM LOGS) AND (id, num) in (SELECT id+2, num FROM LOGS)
+
+with cte as (select player_id, min(event_date) over(partition by player_id) + 1= event_date as is_return from activity)
+select round(sum(is_return)/count(distinct player_id), 2)  as fraction from cte
+
+
